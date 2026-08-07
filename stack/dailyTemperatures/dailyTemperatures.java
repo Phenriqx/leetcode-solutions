e8c9fa0628d01cd2584ad9b2ffa9b164c2a1bead
Phenriqx <pedroxlbd@gmail.com>
2026-08-07 11:27:19 -0300

    daily temperatures

---
+++ stack/dailyTemperatures/dailyTemperatures.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int idx = stack.pop();
+                res[idx] = i - idx;
+            }
+            stack.add(i);
+        }
+
+        return res;
+    }
+}
---
+++ stack/minStack/minStack.java
@@ -0,0 +1,26 @@
+package stack.minStack;
+import java.util.*;
+
+class MinStack {
+
+    public MinStack() {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}
