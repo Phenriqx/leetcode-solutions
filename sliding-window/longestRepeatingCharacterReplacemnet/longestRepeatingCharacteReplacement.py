07ba9de70956076a623874c626e56009a89ec536
Phenriqx <pedroxlbd@gmail.com>
2026-08-31 09:55:34 -0300

    fist and optimized solution for leetcode 424

--- sliding-window/longestRepeatingCharacterReplacemnet/longestRepeatingCharacteReplacement.py
+++ sliding-window/longestRepeatingCharacterReplacemnet/longestRepeatingCharacteReplacement.py
@@ -1,23 +1,25 @@
 class Solution:
+    # first solution i came up with
     def characterReplacementSolution1(self, s: str, k: int) -> int:
         mapp = {}
         i, max_len = 0, 0
-        res = 0
 
         for j in range(len(s)):
             mapp[s[j]] = mapp.get(s[j], 0) + 1
             window_len = j - i + 1
-            max_len = max(max_len, mapp[s[j]])
+            most_frequent = max(mapp.values())
 
-            while window_len - max_len > k:
+            while window_len - most_frequent > k:
                 mapp[s[i]] -= 1
                 i += 1
                 window_len = j - i + 1
+                most_frequent = max(mapp.values())
 
-            res = max(res, window_len)
+            max_len = max(max_len, window_len)
 
-        return res
+        return max_len
 
+    # a little more optimized solution
     def characterReplacementSolution2(self, s: str, k: int) -> int:
         mapp = {}
         i, max_len = 0, 0
